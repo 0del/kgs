70f6a1dcd7f24223bc9c7cdb9a6ff7d5d9076cd0
0del <bany.y0599@gmail.com>
2024-03-22 11:42:00 +0700

    serve

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"crypto/rand"
+	"encoding/json"
+	"math/big"
+	"net/http"
 
 	"github.com/9bany/kgs/pkg/key0"
 )
 
-func main() {
-	gen := key0.NewGen(key0.WithLen(9))
+func keyGen(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	result, _ := rand.Int(rand.Reader, big.NewInt(7))
+	// make sure the lenght always greater than 3 and lessthan 10
+	len := result.Int64() + int64(3)
+	gen := key0.NewGen(key0.WithLen(int8(len)))
 	k, _ := gen.New()
-	fmt.Println(k)
+	resp := map[string]interface{}{
+		"key": k,
+	}
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/key", keyGen)
+	http.ListenAndServe(":8080", nil)
 }
